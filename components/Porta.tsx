0e51d256e15be48b1ce4c9e29603f60da57eebdd
Leticia Lopes <107498937+LeticiaLopess@users.noreply.github.com>
2022-12-19 18:17:48 -0300

    adicionando eventos à porta

--- components/Porta.tsx
+++ components/Porta.tsx
@@ -2,11 +2,11 @@ import PortaModel from "../model/porta"
 import styles from "../styles/Porta.module.css"
 
 interface PortaProps {
-    porta: PortaModel
+    value: PortaModel
 }
 
 export default function Porta(props: PortaProps) {
-    const { porta } = props
+    const porta = props.value
     const selecionada = porta.selecionada ? styles.selecionada : ''
 
     return (
