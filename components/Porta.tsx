39263cd06d38b6cd8b3bead64863dd80f7906763
Leticia Lopes <107498937+LeticiaLopess@users.noreply.github.com>
2022-12-21 15:15:08 -0300

    abrindo a porta

--- components/Porta.tsx
+++ components/Porta.tsx
@@ -1,4 +1,4 @@
-import PortaModel from "../model/porta"
+import PortaModel from '../model/porta'
 import styles from "../styles/Porta.module.css"
 
 interface PortaProps {
@@ -8,17 +8,28 @@ interface PortaProps {
 
 export default function Porta(props: PortaProps) {
     const porta = props.value
-    const selecionada = porta.selecionada ? styles.selecionada : ''
+    const selecionada = porta.selecionada && !porta.aberta? styles.selecionada : ''
 
     const alternarSelecao = e => props.onChange(porta.alternarSelecao())
+    const abrir = e => {
+        e.stopPropagation()
+        props.onChange(porta.abrir())
+    }
+
+    function renderizarPorta() {
+        return (
+                <div className={styles.porta}>
+                    <div className={styles.numero}>{porta.numero}</div>
+                    <div className={styles.macaneta} 
+                    onClick={abrir}></div>
+                </div>
+        )
+    }
 
     return (
         <div className={styles.area} onClick={alternarSelecao}>
             <div className={`${styles.estrutura} ${selecionada}`}>
-                <div className={styles.porta}>
-                    <div className={styles.numero}>{porta.numero}</div>
-                    <div className={styles.macaneta}></div>
-                </div>
+            {porta.aberta ? false : renderizarPorta()}
             </div>
             <div className={styles.chao}></div>
         </div>
--- pages/index.tsx
+++ pages/index.tsx
@@ -7,10 +7,7 @@ export default function Home() {
   const [texto, setTexto] = useState("...")
   return (
     <div style={{display: "flex", flexDirection: "column"}}>
-      <input type="text" value={texto} 
-        onChange={e => setTexto(e.target.value)}/>
       <Porta value={p1} onChange={novaPorta => setP1(novaPorta)}/>
-      
     </div> 
   )
 }
