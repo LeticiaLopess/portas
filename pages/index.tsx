064cccbab36575df1668401844d3fb666451b1a9
Leticia Lopes <107498937+LeticiaLopess@users.noreply.github.com>
2022-12-22 08:39:02 -0300

    renderização de portas: seleção / desmarcar

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,20 +1,22 @@
 import { useState } from "react";
 import Porta from "../components/Porta";
-import { criarPortas } from "../functions/portas";
+import { atualizarPortas, criarPortas } from "../functions/portas";
 import PortaModel from "../model/porta";
 
 export default function Home() {
-  const [p1, setP1] = useState(new PortaModel(1))
-  const [p2, setP2] = useState(new PortaModel(2))
-  const [p3, setP3] = useState(new PortaModel(3))
 
-  console.log(criarPortas(3,2))
+  const [portas, setPortas] = useState(criarPortas(3, 2))
+
+  function renderizarPortas() {
+    return portas.map(porta => {
+      return <Porta key={porta.numero} value={porta} 
+      onChange={ novaPorta => setPortas(atualizarPortas(portas, novaPorta))} />
+    })
+  }
 
   return (
     <div style={{display: "flex"}}>
-      <Porta value={p1} onChange={novaPorta => setP1(novaPorta)}/>
-      <Porta value={p2} onChange={novaPorta => setP2(novaPorta)}/>
-      <Porta value={p3} onChange={novaPorta => setP3(novaPorta)}/>
+      {renderizarPortas()}
     </div> 
   )
 }
