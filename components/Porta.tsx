f0b4c356b0d5f9f59c04f1495939cd8cefa2f6fc
Leticia Lopes <107498937+LeticiaLopess@users.noreply.github.com>
2022-12-22 09:03:17 -0300

    Integração porta presente

--- components/Porta.tsx
+++ components/Porta.tsx
@@ -1,5 +1,6 @@
 import PortaModel from '../model/porta';
 import styles from "../styles/Porta.module.css";
+import Presente from './Presente'
 
 interface PortaProps {
     value: PortaModel
@@ -30,7 +31,9 @@ export default function Porta(props: PortaProps) {
     return (
         <div className={styles.area} onClick={alternarSelecao}>
             <div className={`${styles.estrutura} ${selecionada}`}>
-            {porta.aberta ? false : renderizarPorta()}
+            {porta.fechada ? 
+                renderizarPorta() : 
+                porta.temPresente ? <Presente /> : false}
             </div>
             <div className={styles.chao}></div>
         </div>
